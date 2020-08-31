931402709ed41cb9e4512e29b0ea62b406e2d721
Dessyjerry <jerrycypha@gmail.com>
2020-08-31 06:12:25 -1100

    first commit

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-25-2020.csv"
-
-resp = requests.get(url)
-
-print(resp.text)
